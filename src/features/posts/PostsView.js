0b29970fffb044d0e4081a5d6294fab9a932829a
khaled39ab <khaled39ab@gmail.com>
2022-12-22 23:46:52 +0600

    create a view for posts

---
+++ src/features/posts/PostsView.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PostsView = () => {
+    return (
+        <div style={{ border: '5px solid black', paddingBottom: '3rem', margin: '2rem' }}>
+            <h1>Posts</h1>
+        </div>
+    );
+};
+
+export default PostsView;
