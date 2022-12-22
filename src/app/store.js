d12d59457e20272bb2f5a5b6c6b757fb52c658a5
khaled39ab <khaled39ab@gmail.com>
2022-12-22 23:44:24 +0600

    added posts reducer to store for pass data

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../features/counter/counterSlice";
+import postReducer from "../features/posts/postSlice";
 
 const store = configureStore({
     reducer: {
-        counter: counterReducer // change counterSlice name is valid for export default
+        counter: counterReducer, // change counterSlice name is valid for export default
         // todos: multiple store if needed 
+        posts: postReducer
     }
 });
 
